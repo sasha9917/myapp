4c0c361506d710b5ddec2a398b57e42072120d11
sasha9917 <90724329+sasha9917@users.noreply.github.com>
2025-03-23 11:56:37 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -34,6 +34,15 @@ def add():
     cur.close()
     conn.close()
     return redirect('/')
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        sslmode='require'  # ← ОЦЕ головне!
+    )
+    return conn
 
 # Запуск сервера
 if __name__ == '__main__':
